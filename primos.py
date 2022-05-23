e32dd8d141bfcb7c813673f23254ccef2639abbc
David M <elarreglador@protonmail.com>
2022-05-23 08:27:09 +0200

    Agrego comentarios

--- primos.py
+++ primos.py
@@ -4,19 +4,20 @@
 #
 
 
-
+# Definicion de variables
 i=1
 divisor=0
 primo=True
 
+# Main
 while True:
 	divisor=i-1
-	while divisor > 1:
+	while divisor > 1: 
 		primo=True
-		if i%divisor == 0:
+		if i%divisor == 0: # si da resto 0 no es primo y rompe bucle
 			primo = False
 			break
 		divisor=divisor-1
-	if primo == True:
+	if primo == True: # si termina el bucle es primo
 		print (str(i))
-	i=i+1
+	i=i+1 # pasamos al siguiente numero
